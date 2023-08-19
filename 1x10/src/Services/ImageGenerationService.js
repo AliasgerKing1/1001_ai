a10f980dd617c3bc3d5e699217930ee549a101ba
AliasgerKing1 <aliasgersub34@gmail,com>
2023-08-19 06:02:43 -0700

    changesa

--- 1x10/src/Services/ImageGenerationService.js
+++ 1x10/src/Services/ImageGenerationService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-let apiUrl = "http://localhost:4001/api/image/generate/";
-// let apiUrl = "https://onex10.onrender.com/api/image/generate/";
+// let apiUrl = "http://localhost:4001/api/image/generate/";
+let apiUrl = "https://onex10.onrender.com/api/image/generate/";
 
 let generateImage = async (token, data) => {
 return await axios.post(apiUrl, data, {headers : {token}})
